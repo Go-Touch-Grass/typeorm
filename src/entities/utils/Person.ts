b05f23df20cd7392e00d926b5d39022455d90e8c
Kenneth-Mah <kennethmah@rocketmail.com>
2024-09-10 16:53:13 +0800

    implemented inheritance via Person.ts

---
+++ src/entities/utils/Person.ts
@@ -0,0 +1,29 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Person extends BaseEntity {
+    @PrimaryColumn()
+    id: number;
+
+    @Column()
+    first_name: string;
+
+    @Column()
+    last_name: string;
+
+    @Column({
+        unique: true
+    })
+    email: string;
+
+    @Column({
+        unique: true,
+        length: 10
+    })
+    card_number: string;
+
+    @Column({
+        type: "numeric"
+    })
+    balance: number;
+}
